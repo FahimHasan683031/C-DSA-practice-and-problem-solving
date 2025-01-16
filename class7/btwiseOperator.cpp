e8702b018d05c03c3dfa3955c78ea14faced3ab6
FahimHasan683031 <fahimhasan683031@gmail.com>
2025-01-16 19:15:47 +0600

    btwise operator practice program

--- class7/btwiseOperator.cpp
+++ class7/btwiseOperator.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int binatyTodec (int binaryNum){
-    int ans=0, pow = 1;
 
-    while (binaryNum>0)
-    {
-        int rem = binaryNum %10;
-        binaryNum /=10;
-        ans += (rem*pow);
-        pow *=2;
-    }
-    return ans;
-}
 
 int main(){
-    cout << binatyTodec(101010) << endl;
-    int a= 5;
-    int b=6;
-    char chr = 'A';
 
-    cout << (a & b)<<endl;
+cout << (6 & 10)<<endl;
+cout << (6 | 10)<<endl;
+cout << (6 ^ 10)<<endl;
+cout << (10<<2)<<endl;
+cout << (10>>1)<<endl;
+cout << (10>>2)<<endl;
     
-    for (int i=0; i<5; i++){
-        cout << chr;
-        chr++;
-    }
+    
+    
 }
