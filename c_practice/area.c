ae05e52409f2ee5ee59a9a7f0f711c6e1db93b6a
FahimHasan683031 <fahimhasan683031@gmail.com>
2025-01-22 23:20:08 +0600

    alculate area program

---
+++ c_practice/area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int length,width;
+    printf("Enter the value of length");
+    scanf("%d",&length);
+
+    printf("Enter the value of width");
+    scanf("%d", &width);
+
+
+    printf("Area is : %d", length*width);
+}
