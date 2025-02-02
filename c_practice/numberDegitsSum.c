34443dfca53551a3fb79e2d7099b5b9de556af5d
FahimHasan683031 <fahimhasan683031@gmail.com>
2025-02-02 22:13:07 +0600

    solbe the problem in the sum function

--- c_practice/numberDegitsSum.c
+++ c_practice/numberDegitsSum.c
@@ -3,9 +3,9 @@
 int sumOfdegist(int num);
 
 int main(){
-    int result =sumOfdegist(122);
+    int result =sumOfdegist(1225862556);
 
-printf("%d" ,&result);
+printf("%d\n" ,result);
     return 0;
 }
 
@@ -14,8 +14,8 @@ int sumOfdegist (int num){
     while (num>0)
     {
     int lastDgt = num%10;
-    sum = sum + lastDgt;
-    num = num/10; 
+    sum += lastDgt;
+    num = num/10;
 
     }
     
